ec25c31b97f5498583a432052673d29415c83e8f
Naren <kumarnarendiran2000@gmail.com>
2025-01-21 20:32:00 +0530

    install gluestack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,11 @@
-import {Stack} from "expo-router"
+import "@/global.css";
+import { Stack } from "expo-router";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 
-export default function RootLayout(){
-    return <Stack />
+export default function RootLayout() {
+  return (
+    <GluestackUIProvider>
+      <Stack />;
+    </GluestackUIProvider>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,16 @@
 import { Text, FlatList } from "react-native";
 import products from "../assets/products.json";
 import ProductListItem from "../components/ProductListItem";
+import { Button, ButtonText } from "@/components/ui/button";
 
 export default function HomeScreen() {
   return (
-    <FlatList
-      data={products}
-      renderItem={({ item }) => <ProductListItem product={item} />}
-    />
+    <Button>
+      <ButtonText>Press me</ButtonText>
+    </Button>
+    // <FlatList
+    //   data={products}
+    //   renderItem={({ item }) => <ProductListItem product={item} />}
+    // />
   );
 }
